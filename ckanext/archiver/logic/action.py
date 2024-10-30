b7026252cef8d5ec1c9b1451e69e0df394a3b778
Momin Imtiaz <momin@echo.local>
2024-10-30 17:31:32 +0300

    temp

--- ckanext/archiver/logic/action.py
+++ ckanext/archiver/logic/action.py
@@ -3,6 +3,7 @@ import logging
 import ckan.plugins as p
 from ckan import model
 from ckanext.archiver.model import Archival, aggregate_archivals_for_a_dataset
+from ckanext.archiver import tasks
 
 ObjectNotFound = p.toolkit.ObjectNotFound
 _get_or_bust = p.toolkit.get_or_bust
@@ -46,3 +47,37 @@ def archiver_dataset_show(context, data_dict=None):
     archival_dict = aggregate_archivals_for_a_dataset(archivals)
     p.toolkit.check_access('archiver_dataset_show', context, data_dict)
     return archival_dict
+
+
+def archiver_package_show_or_initiate(context, data_dict):
+    id_ = _get_or_bust(data_dict, 'id')
+
+    p.toolkit.check_access('archiver_package_show_or_initiate', context, data_dict)
+
+    dataset = model.Package.get(id_)
+    if not dataset:
+        raise ObjectNotFound
+    
+    archivals = Archival.get_for_package(dataset.id)
+
+    if not archivals:
+        tasks.update_package(dataset.id)
+
+    return archiver_dataset_show(context, data_dict)
+
+
+def archiver_resource_show_or_initiate(context, data_dict):
+    id_ = _get_or_bust(data_dict, 'id')
+
+    p.toolkit.check_access('archiver_resource_show_or_initiate', context, data_dict)
+
+    resource = model.Resource.get(id_)
+    if not resource:
+        raise ObjectNotFound
+    
+    archivals = Archival.get_for_resource(resource.id)
+
+    if not archivals:
+        tasks.update_resource(resource.id)
+
+    return archiver_resource_show(context, data_dict)
